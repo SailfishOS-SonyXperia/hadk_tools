bed0eae5860b95d9da0b44e67541bc0b75c98974
Björn Bidar <theodorstormgrade@gmail.com>
2019-10-11 05:21:57 +0200

    hadk.source: check if sync function is defined first.

--- src/hadk.source.in.sh
+++ src/hadk.source.in.sh
@@ -3,7 +3,18 @@
 #\\include hadk.base.in.sh
 
 
-if ! depend "$device_file" sync ; then
+seperate_chainload()
+# desc: seperate each chainload by running in a seperate instance for each job
+{
+    if is_function sync ; then
+        if ! sync ; then
+            die "Error exit status $exit_status in $device_file:$1:sync()"
+        fi
+    fi
+}
+
+
+if ! depend "$device_file" seperate_chainload ; then
     error "$HADK_FILE_NOT_FOUND not found"
     exit 1
 fi
