5fdf3dd6473416027a713f3b70a25ace707605b6
Björn Bidar <theodorstormgrade@gmail.com>
2020-08-12 16:03:47 +0300

    hadk.base.env: use $HOME insteda of ~c

--- src/hadk.base.env.in.sh
+++ src/hadk.base.env.in.sh
@@ -1,4 +1,4 @@
 appname=${0##*/}
-CONFIGDIR="${XDG_DATA_HOME:-~/.local/share}/hadk"
+CONFIGDIR="${XDG_DATA_HOME:-$HOME/.local/share}/hadk"
 depend_path="$CONFIGDIR"/devices:"$CONFIGDIR"
 #\\define EXPORT_VAR_PREFIX HADK
