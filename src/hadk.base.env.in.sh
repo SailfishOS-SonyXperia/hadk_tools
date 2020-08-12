bfc08ada5b8979a9e8bc0d89508046f2e36e3f01
Björn Bidar <theodorstormgrade@gmail.com>
2020-08-12 11:52:36 +0300

    hadk.base.env: Add $configdir to $depend_path

--- src/hadk.base.env.in.sh
+++ src/hadk.base.env.in.sh
@@ -1,4 +1,4 @@
 appname=${0##*/}
 CONFIGDIR="${XDG_DATA_HOME:-~/.local/share}/hadk"
-depend_path=$depend_path:"$CONFIGDIR"/devices
+depend_path="$CONFIGDIR"/devices:"$CONFIGDIR"
 #\\define EXPORT_VAR_PREFIX HADK
