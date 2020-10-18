404bd10b8e5dbebbca87a42dc776ca371496ccae
Björn Bidar <theodorstormgrade@gmail.com>
2020-10-18 07:36:40 +0300

    hadk.base.env: Always add $PWD to $depend_path

--- src/hadk.base.env.in.sh
+++ src/hadk.base.env.in.sh
@@ -1,4 +1,4 @@
 appname=${0##*/}
 CONFIGDIR="${XDG_DATA_HOME:-$HOME/.local/share}/hadk"
-depend_path="$CONFIGDIR"/devices:"$CONFIGDIR"
+depend_path="$CONFIGDIR"/devices:"$CONFIGDIR":"$PWD"
 #\\define EXPORT_VAR_PREFIX HADK
