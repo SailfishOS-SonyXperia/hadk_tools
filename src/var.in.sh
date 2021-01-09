b9e07a2be77fb2fbab78c2ef97589774b20b3e65
Björn Bidar <theodorstormgrade@gmail.com>
2021-01-09 17:00:56 +0200

    var: Use proper variable name.

--- src/var.in.sh
+++ src/var.in.sh
@@ -11,27 +11,27 @@ var()
 	*=|*=*)
 	    local __var_part1=$( echo "$1" | sed -e 's/=.*//' -e 's/^[+,-]//' )
             local __var_part2=$( echo "$1" | cut -d '=' -f2- )
-	    local __var12="@VAR_DIR@/$__var_part1"
+	    local __var12="@VARDB_DIR@/$__var_part1"
 	    mkdir -p "${__var12%/*}"
 	    case $1 in
 		*+=*)
-		    if [ -d "@VAR_DIR@/$__var_part1" ] ; then
-			printf  '%s' $__var_part2 > "@VAR_DIR@/$__var_part1/"\  $((
-				$( echo "@VAR_DIR@"/$__var_part2/* \
+		    if [ -d "@VARDB_DIR@/$__var_part1" ] ; then
+			printf  '%s' $__var_part2 > "@VARDB_DIR@/$__var_part1/"\  $((
+				$( echo "@VARDB_DIR@"/$__var_part2/* \
 				    | tail  | xargs basename ) + 1 ))
 		    else
-			printf '%s' "$__var_part2" >> "@VAR_DIR@/$__var_part1"
+			printf '%s' "$__var_part2" >> "@VARDB_DIR@/$__var_part1"
 		    fi
 		    ;;
  		*-=*) false ;;
-                *)  printf '%s' "$__var_part2" > "@VAR_DIR@/$__var_part1" ;;
+                *)  printf '%s' "$__var_part2" > "@VARDB_DIR@/$__var_part1" ;;
 	    esac
 	    ;;
 	*)
-	    if [ -d "@VAR_DIR@/$1" ] ; then
-                ls -1v "@VAR_DIR@/$1"
-	    elif [ -e "@VAR_DIR@/$1" ] ; then
-		cat "@VAR_DIR@/$1"
+	    if [ -d "@VARDB_DIR@/$1" ] ; then
+                ls -1v "@VARDB_DIR@/$1"
+	    elif [ -e "@VARDB_DIR@/$1" ] ; then
+		cat "@VARDB_DIR@/$1"
 	    else
 		return 1
 	    fi
