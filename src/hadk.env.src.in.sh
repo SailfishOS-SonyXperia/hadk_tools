49b18b7b73f2502509c0d38901c5a218ffcd7d35
Björn Bidar <bjorn.bidar@thaodan.de>
2022-02-14 12:36:58 +0200

    templates.hadk.env.src: Update to latest release

--- src/hadk.env.src.in.sh
+++
@@ -1,2 +0,0 @@
-UBUCHRT_URL=http://img.merproject.org/images/mer-hybris/ubu/ubuntu-trusty-android-rootfs.tar.bz2
-SFOSSDK_URL=http://releases.sailfishos.org/sdk/installers/latest/Jolla-latest-SailfishOS_Platform_SDK_Chroot-i486.tar.bz2
